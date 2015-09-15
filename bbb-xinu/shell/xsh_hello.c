f4e5b077d9794f6fcb6405e167389802baa23125
lbsharma <lbsharma@iu.edu>
2015-09-15 01:13:30 -0400

    Update xsh_hello.c

--- bbb-xinu/shell/xsh_hello.c
+++ bbb-xinu/shell/xsh_hello.c
@@ -1,1 +1,22 @@
 
+/* xsh_date.c - xsh_date */
+
+#include <xinu.h>
+#include <string.h>
+#include <stdio.h>
+
+/*------------------------------------------------------------------------
+ * xsh_date - obtain and print the current month, day, year, and time
+ *------------------------------------------------------------------------
+ */
+shellcmd xsh_hello(int nargs, char *args[]) {
+if(nargs==2)
+{
+  printf("Hello %s, Welcome to the worls of Xinu!!\n", args[1])
+}
+else
+{
+ printf("Hello User, Welcome to the world of Xinu\n")  
+}
+	return 0;
+}
