d5954b527d56518b0d910a7991229c6928c08df5
lbsharma <lbsharma@iu.edu>
2015-09-17 22:04:53 -0400

    Update xsh_hello.c

--- bbb-xinu/shell/xsh_hello.c
+++ bbb-xinu/shell/xsh_hello.c
@@ -1,22 +1,51 @@
-
-/* xsh_date.c - xsh_date */
+/************************************************************************/
+/*		Submitted by : Aritra De and Leeladhar Sharma		*/
+/*		Assignment 1 | Advanced Operating Systems		*/
+/*		Submitted On : 17.09.2015				*/
+/************************************************************************/
+/* xsh_hello.c - xsh_hello */
 
 #include <xinu.h>
 #include <string.h>
 #include <stdio.h>
+#include <ctype.h>
 
 /*------------------------------------------------------------------------
- * xsh_date - obtain and print the current month, day, year, and time
+ * xsh_hello - print Hello world
  *------------------------------------------------------------------------
  */
+
 shellcmd xsh_hello(int nargs, char *args[]) {
-if(nargs==2)
-{
-  printf("Hello %s, Welcome to the worls of Xinu!!\n", args[1]);
-}
-else
-{
- printf("Hello User, Welcome to the world of Xinu\n");
-}
+int i=0,k=0;
+	if(nargs==2)
+		{       
+			while(i<strlen(args[1]))
+                         {
+                            
+			    if(isalpha(args[1][i])==0)
+				{	
+					printf("Special characters and digits not allowed\n");
+					break;
+				}
+
+			    else
+				k++;
+				i++;
+
+			}	
+	if(k==strlen(args[1]))
+		printf("Hello %s, Welcome to the world of Xinu!!\n", args[1]);
+		
+  			
+		}
+	else if(nargs>2)
+		{
+ 			printf("Too many arguments\n");	
+		}
+	else
+		{
+			 printf("Too little arguments\n");
+		}	
+
 	return 0;
 }
