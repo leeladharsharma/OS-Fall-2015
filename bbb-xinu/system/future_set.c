58e9b23aa9ead936ae34fb094e5d3b0ab117fbc9
lbsharma <lbsharma@iu.edu>
2015-10-13 23:16:24 -0400

    Update future_set.c

--- bbb-xinu/system/future_set.c
+++ bbb-xinu/system/future_set.c
@@ -1,14 +1,18 @@
-
+//#include <xinu.h>
 #include <future.h>
 /* if future does not have value set */
 syscall future_set(future *f, int *i) 
 {
+  intmask mask;
+  mask = disable();
   if (f->state == FUTURE_EMPTY || f->state == FUTURE_WAITING)
   {
-    printf("value set\n");
-    f->value = (int*)*i;
+    f->value = i;
     f->state = FUTURE_VALID; 
+    resume(f->pid);
+    restore(mask);
     return OK; 
   } 
+  restore(mask);
   return SYSERR;
 }
