d4a13bf458bdabca85515dc5697c50d2b34e64fc
lbsharma <lbsharma@iu.edu>
2015-10-23 23:20:07 -0400

    Update future_alloc.c

--- bbb-xinu/system/future_alloc.c
+++ bbb-xinu/system/future_alloc.c
@@ -1,15 +1,25 @@
-//#include <xinu.h>
+
 #include <future.h>
 future* future_alloc(int future_flags)
 {
   register future* f;
   f = (future*)getmem(sizeof(future));
+
 if(f != SYSERR)
 {
+ if(f->flag == FUTURE_QUEUE)
+{
+    	f->set_head = -1;
+	f->set_tail= -1;
+	f->get_head = -1;
+	f->get_tail = -1;
+}
   f->flag= future_flags;
   f->state = FUTURE_EMPTY;
+
   return f;
 }
+
  return SYSERR;
 }
 
