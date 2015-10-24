611322831e7b7ed0216129310465fe4c84ea946a
lbsharma <lbsharma@iu.edu>
2015-10-23 23:22:52 -0400

    Update future_get.c

--- bbb-xinu/system/future_get.c
+++ bbb-xinu/system/future_get.c
@@ -1,9 +1,7 @@
-
 #include <future.h>
-/* if future has value return */
-syscall future_get(future *f, int *value)
-{
 
+syscall future_get(future *f, int* value)
+{	 
 intmask mask;
 
 if(f->state == FUTURE_EMPTY || f->state == FUTURE_EXCLUSIVE)
@@ -29,4 +27,47 @@ if(f->state == FUTURE_EMPTY || f->state == FUTURE_EXCLUSIVE)
   restore(mask);
   return SYSERR;
 }
+	if(f->state ==FUTURE_EMPTY && f->flag == FUTURE_QUEUE)
+	{
+		//Add it to the get queue
+		intmask mask = disable();
+
+		//SET EMPTY
+		if(isQueueEmpty(&(f->set_head),&(f->set_tail)))
+		{
+			enQueue(f->get_queue,getpid(),&(f->get_head),&(f->get_tail));
+			suspend(getpid());
+			*value = f->value;	
+		}
+		else
+		{
+			pid32 tpid;			
+			tpid=deQueue(f->set_queue, &(f->set_head), &(f->set_tail));
+                        resume(tpid);
+			*value = f->value;	
+			return OK;
+		}
+
+		restore(mask);
+		return OK;
+	}
+	
+if(f->flag == FUTURE_SHARED)
+    {
+
+      if(f->state == FUTURE_EMPTY)
+      {
+          enQueue(f->get_queue,getpid(),&(f->get_head),&(f->get_tail));      // enqueue
+          suspend(getpid());
+      //return OK;
+      }
+      if(f->state == FUTURE_VALID)
+      {
+          *value = f->value;    
+          return OK;
+      }
+
+    }
+	
+return SYSERR;
 }
